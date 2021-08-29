0fa465974c9379e7ac372150f8a5b0585bd165d1
jagruth vasa <60261491+jagruthvasa@users.noreply.github.com>
2021-08-29 07:36:44 +0530

    Update 2-jtable.java

--- 2-jtable.java
+++ 2-jtable.java
@@ -1,11 +1,11 @@
 import javax.swing.*;    
 class abc extends JFrame{    
     JFrame f;    
-    public i(){    
+    abc(){    
     f=new JFrame();    
     String data[][]={ {"101","Mahesh","9515479704"},    
                           {"102","Akshay","8978937353"},    
-                          {"103","Suchi","9573299464"}
+                          {"103","Suchi","9573299464"},
                         {"104","Jagruth","9010345678"}};    
     String column[]={"ID","NAME","MOBILE NUMBER"};         
     JTable jt=new JTable(data,column);    
@@ -15,9 +15,10 @@ class abc extends JFrame{
     setSize(300,400);    
     setVisible(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        new abc();
+    }
 }
-public class i{
-    public static void main(String[] args) {    
-   abc obj=new abc();    
- }    
-}  
